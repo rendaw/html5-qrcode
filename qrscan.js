695750d0ac44e6ff4793d417cf5b0000302cefdf
rendaw <spoo@zarbosoft.com>
2017-11-30 03:31:14 +0900

    Scaling video, stop fix

--- qrscan.js
+++ qrscan.js
@@ -2,65 +2,77 @@ import qrcode from './lib/qrcode.js'
 
 const qrscan = {
 	start: (mount, qrcodeSuccess, qrcodeError, videoError) => {
-		let width = mount.clientWidth
-		let height = mount.clientHeight
-
-		if (width == null) {
-			width = 300
-		}
-
-		if (height == null) {
-			height = 250
-		}
-
 		const video = document.createElement('video')
-		video.setAttribute('width', String(width) + 'px')
-		video.setAttribute('height', String(height) + 'px')
 		video.setAttribute('autoplay', '')
+		video.setAttribute('width', '100%')
 		mount.appendChild(video)
-		const canvas = document.createElement('canvas')
-		canvas.setAttribute('width', String(width) + 'px')
-		canvas.setAttribute('height', String(height) + 'px')
-		canvas.style.display = 'none'
-		mount.appendChild(canvas)
 
-		const context = canvas.getContext('2d')
 		const data = {
+			canceled: false,
 			stream: null,
+			timeout: null,
 		}
 
-		const scan = () => {
-			if (data.stream === null) return
-			context.drawImage(video, 0, 0, width, height)
-			try {
-				qrcode.decode(canvas)
-			} catch (e) {
-				qrcodeError(e, data.stream)
+		setTimeout(() => {
+			if (data.canceled) {
+				return
 			}
-		}
+			let width = mount.clientWidth
+			let height = mount.clientHeight
 
-		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+			if (width == null) {
+				width = 300
+			}
 
-		const successCallback = stream => {
-			video.src = window.URL.createObjectURL(stream)
-			data.stream = stream
-			video.play()
-			data.timeout = setInterval(scan, 500)
-		}
+			if (height == null) {
+				height = 250
+			}
 
-		navigator.getUserMedia({video: true}, successCallback, error => {
-			videoError(error, data.stream)
-		})
+			const canvas = document.createElement('canvas')
+			canvas.setAttribute('width', String(width) + 'px')
+			canvas.setAttribute('height', String(height) + 'px')
+			canvas.style.display = 'none'
+			mount.appendChild(canvas)
 
-		qrcode.callback = result => {
-			qrcodeSuccess(result, data.stream)
-		}
+			const context = canvas.getContext('2d')
+
+			const scan = () => {
+				if (data.stream === null) return
+				context.drawImage(video, 0, 0, width, height)
+				try {
+					qrcode.decode(canvas)
+				} catch (e) {
+					qrcodeError(e, data.stream)
+				}
+			}
+
+			navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
+
+			const successCallback = stream => {
+				video.src = window.URL.createObjectURL(stream)
+				data.stream = stream
+				video.play()
+				data.timeout = setInterval(scan, 500)
+			}
+
+			navigator.getUserMedia({video: true}, successCallback, error => {
+				videoError(error, data.stream)
+			})
+
+			qrcode.callback = result => {
+				qrcodeSuccess(result, data.stream)
+			}
+		}, 0)
 
 		return data
 	},
 	stop: data => {
-		data.stream.getVideoTracks().forEach(track => track.stop())
-		clearInterval(data.timeout)
+		data.canceled = true
+		// Null if initialization failed (ex: no camera)
+		if (data.stream !== null)
+			data.stream.getVideoTracks().forEach(track => track.stop())
+		if (data.timeout !== null)
+			clearInterval(data.timeout)
 	},
 }
 
